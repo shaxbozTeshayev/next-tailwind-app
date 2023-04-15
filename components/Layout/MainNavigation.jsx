6072d8bd1108ef737a8c03e8d1c80d2ed01e49ce
Shakhboz Teshaev <shaxbozteshayev7333@gmail.com>
2023-04-15 14:43:56 +0500

    ss

--- components/Layout/MainNavigation.jsx
+++ components/Layout/MainNavigation.jsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import MobileNav from "../MobileNav";
+import Modal from "../Modal";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const MainNavigation = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOnClose = () => setShowModal(false);
+
   const headerNavLinks = [
     { href: "/internet", title: "Интернет" },
     { href: "/business", title: "Для бизнес" },
@@ -10,46 +17,61 @@ const MainNavigation = () => {
     { href: "/contact", title: "Контакты" },
   ];
 
-  return (
-    <header className="flex h-16 w-full items-center justify-around bg-[#FFFFFF] px-6 md:h-20 md:justify-between md:px-20 md:py-6  xl:h-24  xl:py-[2.5rem] xl:px-44">
-      <div className="">
-        <Link href="/">
-          <Image
-            src="/images/UzCloud.png"
-            priority
-            width={166}
-            height={43}
-            alt="Nav"
-          />
-        </Link>
-      </div>
-      <nav>
-        <div className="hidden justify-center md:flex md:items-center md:gap-4 xl:gap-10">
-          {headerNavLinks.map((item, idx) => (
-            <Link
-              key={idx}
-              href={item.href}
-              className="md:text-md font-semibold italic text-coolGray hover:text-mainColor"
-            >
-              {item.title}
-            </Link>
-          ))}
+  const router = useRouter();
+  const { pathname } = router;
 
-          <div className="flex gap-3.5">
+  return (
+    <>
+      <header className="xl:px-34 flex h-16 w-full items-center justify-around bg-[#FFFFFF] px-6 md:h-20 md:justify-between md:px-20  md:py-6  xl:h-24 xl:py-10">
+        <div className="">
+          <Link href="/">
             <Image
-              src="/icons/internet.svg"
-              alt="search"
-              width="20"
-              height="20"
+              src="/images/UzCloud.png"
+              priority
+              width={166}
+              height={43}
+              alt="Nav"
             />
-            <p className="md:text-md font-semibold uppercase italic text-mainColor">
-              Uz
-            </p>
-          </div>
+          </Link>
         </div>
-        <MobileNav />
-      </nav>
-    </header>
+        <nav>
+          <div className="hidden justify-center md:flex md:items-center md:gap-4 xl:gap-10">
+            {headerNavLinks.map((item, idx) => (
+              <Link
+                key={idx}
+                href={item.href}
+                className={`${
+                  item.href === pathname ? "text-mainColor" : "text-coolGray"
+                } md:text-md font-semibold italic text-coolGray hover:text-mainColor`}
+              >
+                {item.title}
+              </Link>
+            ))}
+
+            <div className="flex items-center justify-center gap-3.5">
+              <button
+                onClick={() => setShowModal(true)}
+                className="rounded-md bg-white py-2 px-3 text-sm text-mainColor"
+              >
+                Личный кабинет
+              </button>
+              <Image
+                src="/icons/internet.svg"
+                alt="search"
+                width="20"
+                height="20"
+              />
+              <p className="md:text-md font-semibold uppercase italic text-mainColor">
+                Uz
+              </p>
+            </div>
+          </div>
+
+          <MobileNav />
+        </nav>
+      </header>
+      <Modal onClose={handleOnClose} visible={showModal} />
+    </>
   );
 };
 export default MainNavigation;
---
+++ components/Modal.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+const Modal = ({ visible, onClose }) => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleOnClose = (e) => {
+    if (e.target.id === "modal") onClose();
+  };
+
+  if (!visible) return null;
+  return (
+    <div
+      id="modal"
+      onClick={handleOnClose}
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 backdrop-blur-sm"
+    >
+      {/* <div className="flex flex-col items-center rounded-lg bg-white py-20 px-32">
+        <div className="text-2xl font-medium text-textMain">
+          Введите свой номер телефона
+        </div>
+        <p className="mt-5 mb-10 text-base font-medium text-coolGray">
+          Для входа в личный кабинет введите свой номер телефона
+        </p>
+
+        <PhoneInput
+          country={"uz"}
+          value={phoneNumber}
+          onChange={setPhoneNumber}
+        />
+
+        <button
+          type="button"
+          className="mt-16 w-full cursor-pointer rounded-md  bg-mainColor py-4 text-center text-xl text-white shadow-md hover:opacity-80"
+        >
+          Отправка
+        </button>
+      </div> */}
+
+      <div className="flex flex-col items-center rounded-lg bg-white py-20 px-32">
+        <div className="text-2xl font-medium text-textMain">
+          SMS-подтверждение
+        </div>
+        <p className="mt-5 mb-10 text-base font-medium text-coolGray">
+          Мы отправили SMS на номер +998 9* *** ** ** Введите 5-значный код
+        </p>
+
+        <PhoneInput
+          country={"uz"}
+          value={phoneNumber}
+          onChange={setPhoneNumber}
+        />
+
+        <button
+          type="button"
+          className="mt-16 w-full cursor-pointer rounded-md  bg-mainColor py-4 text-center text-xl text-white shadow-md hover:opacity-80"
+        >
+          Отправка
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
