9bde13cb9dd0d55a92853f48908c856d381b2389
Shakhboz Teshaev <shaxbozteshayev7333@gmail.com>
2023-04-26 13:35:10 +0500

    sss

--- pages/authUser/index.jsx
+++ pages/authUser/index.jsx
@@ -2,19 +2,11 @@ import { useSession } from "next-auth/react";
 import React from "react";
 
 const AuthUser = () => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   // console.log(session?.user);
   return (
     <div>
       <h1>Logged In</h1>
-      {session?.user ? (
-        <>
-          <h1>{session.user.name}</h1>
-          <button>Sign Out</button>
-        </>
-      ) : (
-        <button>Sign In</button>
-      )}
     </div>
   );
 };
